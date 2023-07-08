54a62016c00f289068035db1603a9359de1a7899
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-07-08 02:22:52 +0100

    Delete 0x00-python_variable_annotations directory

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
- """ type-annotated function floor which takes a float n as argument
-        and returns the floor of the float. """
-
-def floor(n: float) -> float:
-  return int(n)
